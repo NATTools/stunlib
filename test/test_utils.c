0df7d5db4a7504ed37f783246d5b899c9078b9ae
Pål-Erik Martinsen <palerikm@gmail.com>
2015-07-18 15:06:10 +0200

    More needed files.

---
+++ test/test_utils.c
@@ -0,0 +1,19 @@
+/**
+ * \file
+ *
+ * Copyright (c) 2015 sockaddrutil authors.  See LICENSE file.
+ */
+
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CTEST_MAIN
+#include "test_utils.h"
+
+int main(int argc, const char *argv[])
+{
+    return ctest_main(argc, argv);
+}
+
---
+++ test/test_utils.h
@@ -0,0 +1,13 @@
+/**
+ * \file
+ * test_utils.h
+ *
+ * Copyright (c) 2015 sockaddrutil authors.  See LICENSE file.
+ */
+
+
+#pragma once
+
+#define CTEST_SEGFAULT
+#include "ctest.h"
+
